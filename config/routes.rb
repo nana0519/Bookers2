36d05c34294ffcfb1216715ea4d7f456eaade100
nana0519 <95403153+nana0519@users.noreply.github.com>
2022-01-19 04:40:47 +0000

    [Finish]ファイルの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
-  get 'homes/about'
+  root to: "homes#top"
+  get "home/about" => "homes#about", as: ""
+  resources :books, only:[:index, :show, :edit]
+  resources :users, only:[:index, :show]
+  
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

09e25668552b5dfea782e4fbced6af01b312cced
nana0519 <95403153+nana0519@users.noreply.github.com>
2022-01-22 15:28:38 +0000

    [Finish]バリデーションの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,8 +2,14 @@ class BooksController < ApplicationController
   def create
 		@book = Book.new(book_params)
 		@book.user_id = current_user.id
-	  @book.save
-	  redirect_to book_path(@book)
+	 if @book.save
+	    flash[:notice] = "You have created book successfully."
+	    redirect_to book_path(@book)
+	 else
+	    @user = current_user
+      @books = Book.all
+      render :index
+	 end
   end
   
   def index
@@ -23,8 +29,12 @@ class BooksController < ApplicationController
   
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book)
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
   end
   
   def destroy

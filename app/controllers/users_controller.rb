e794c9590855df8c067b85ef4d9dce5dffa04ff7
nana0519 <95403153+nana0519@users.noreply.github.com>
2022-01-23 06:19:20 +0000

    [Finish]4-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,13 +20,13 @@ class UsersController < ApplicationController
   
   def update
     @user = current_user
-     if @user.update(user_params)
+    if @user.update(user_params)
         flash[:notice] = "You have updated user successfully."
         redirect_to user_path(@user.id)
-      else
+    else
         render :edit
       
-     end
+    end 
   end
 
   private
